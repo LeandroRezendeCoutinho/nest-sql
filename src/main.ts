8324cbd16673dedf164df0d8f88d82b37f6adaa7
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2023-04-14 14:15:41 -0300

    clusterize application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+import cluster from 'node:cluster';
+import { cpus } from 'os'
+import { Logger } from '@nestjs/common'
+
+if (cluster.isPrimary) {
+  const cpuCount = cpus().length
+
+  for (let i = 0; i < cpuCount -2; i += 1) {
+    cluster.fork()
+  }
+
+  cluster.on('online', worker => {
+    Logger.log('Worker ' + worker.process.pid + ' is online.')
+  })
+  cluster.on('exit', ({ process }) => {
+    Logger.log('worker ' + process.pid + ' died.')
+  })
+} else {
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  }
+  bootstrap();
 }
-bootstrap();
